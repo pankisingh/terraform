33f89b2c3b33b7a3960bfa72cd66288dc1d8a653
Pankaj Singh <Pankisingh070@gmail.com>
2023-01-31 22:42:25 +0530

    types added

--- 02-veriables/var.tf
+++ 02-veriables/var.tf
@@ -12,3 +12,20 @@ variable "sample2" {
 output "sample2" {
   value = var.sample2
 }
+
+variable "string" {
+  default = "Hello"
+}
+
+variable "number" {
+  default = 10
+}
+
+variable "boolean" {
+  default = false
+}
+
+
+output "data.types" {
+  value = "string = ${var.string}, boolean =  ${var.boolean}, number = ${var.number}"
+}
