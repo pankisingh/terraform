80d5dcfcf96afef666be8994837c539e8eed999f
Pankaj Singh <Pankisingh070@gmail.com>
2023-02-01 23:26:09 +0530

    ec2

--- 03-sample/ec2.tf
+++ 03-sample/ec2.tf
@@ -1,12 +1,12 @@
-# resource "aws_instance" "sample" {
-#   ami                    = "ami-074df373d6bafa625"
-#   instance_type          = "t3.micro"
-#   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+resource "aws_instance" "sample" {
+  ami                    = "ami-074df373d6bafa625"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
-#   tags = {
-#     Name = "sample"
-#   }
-# }
+  tags = {
+    Name = "sample"
+  }
+}
 
 
 resource "aws_security_group" "allow_ssh" {
