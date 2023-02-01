03c287057b5c755496c805993186611b75fcd4ae
Pankaj Singh <Pankisingh070@gmail.com>
2023-02-01 22:54:57 +0530

    ec2

---
+++ 03-sample/ec2.tf
@@ -0,0 +1,44 @@
+# resource "aws_instance" "sample" {
+#   ami                    = "ami-074df373d6bafa625"
+#   instance_type          = "t3.micro"
+#   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+#   tags = {
+#     Name = "sample"
+#   }
+# }
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+# output "ec2-attributes" {
+#   value = aws_instance.sample
+# }
+
+provider "aws" {
+  region = "us-east-1"
+}
+  
