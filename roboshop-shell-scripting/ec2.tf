3eec81084cbdcfc0aedc2ca9c7a6cbf8193c306d
Pankaj Singh <Pankisingh070@gmail.com>
2023-02-02 23:44:49 +0530

    ec2

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "cheap_worker" {
   count                     = local.LENGTH
   //count                     = length(var.COMPONENTS)
-  ami                       = "ami-074df373d6bafa625"
+  ami                       = "ami-0bb6af715826253bf"
   spot_price                = "0.0031"
   instance_type             = "t3.micro"
   vpc_security_group_ids    = ["sg-0017bec6ae883767e"]
