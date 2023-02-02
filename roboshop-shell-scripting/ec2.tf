0d262bd9081cc430937759be9caa2e38455029f9
Pankaj Singh <Pankisingh070@gmail.com>
2023-02-02 23:42:14 +0530

    ec2

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -1,23 +1,23 @@
-# resource "aws_spot_instance_request" "cheap_worker" {
-#   count                     = local.LENGTH
-#   //count                     = length(var.COMPONENTS)
-#   ami                       = "ami-074df373d6bafa625"
-#   spot_price                = "0.0031"
-#   instance_type             = "t3.micro"
-#   vpc_security_group_ids    = ["sg-0017bec6ae883767e"]
-#   wait_for_fulfillment      = true
-#   //spot_type                 = "persistent"
-#   tags                      = {
-#     Name                    = element(var.COMPONENTS, count.index)
-#   }
-# }
+resource "aws_spot_instance_request" "cheap_worker" {
+  count                     = local.LENGTH
+  //count                     = length(var.COMPONENTS)
+  ami                       = "ami-074df373d6bafa625"
+  spot_price                = "0.0031"
+  instance_type             = "t3.micro"
+  vpc_security_group_ids    = ["sg-0017bec6ae883767e"]
+  wait_for_fulfillment      = true
+  //spot_type                 = "persistent"
+  tags                      = {
+    Name                    = element(var.COMPONENTS, count.index)
+  }
+}
 
-# resource "aws_ec2_tag" "name-tag" {
-#   count                     = local.LENGTH
-#   resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-#   key                       = "Name"
-#   value                     = element(var.COMPONENTS, count.index)
-# }
+resource "aws_ec2_tag" "name-tag" {
+  count                     = local.LENGTH
+  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+  key                       = "Name"
+  value                     = element(var.COMPONENTS, count.index)
+}
 
 
 resource "aws_route53_record" "records" {
